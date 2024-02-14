cd2cd1e230155669fd931b9b5b2d580b7299ceeb
jani.maenpaa <jani.maenpaa@aalto.fi>
2024-02-14 12:34:33 +0200

    Edit returned event object

--- src/utils/ical.ts
+++ src/utils/ical.ts
@@ -1,6 +1,6 @@
 import ical, { CalendarComponent, VEvent } from 'node-ical'
 
-const filteredEventTypes = ['kahvitus', 'kokous']
+const filteredEventTypes = ['kahvitus', 'kokous', 'kahvit']
 
 export const parseIcal = async (url: string) => {
   try {
@@ -9,13 +9,14 @@ export const parseIcal = async (url: string) => {
     return Object.values(events)
       .filter((event: CalendarComponent): event is VEvent => {
         if ('type' in event && event.type === 'VEVENT') {
-          return !filteredEventTypes.some((type) => event.summary.includes(type))
+          return !filteredEventTypes.some((type) => event.summary.toLowerCase().includes(type))
         }
         return false
       })
       .map((event: VEvent) => ({
         name: event.summary,
-        date: { startDate: event.start.toISOString(), endDate: event.end.toISOString() },
+        startTime: event.start.toISOString(),
+        endTime: event.end.toISOString(),
         description: event.description,
         url: event.url,
       }))
