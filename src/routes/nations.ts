4ba076c72a3c8a760079c42977afb1f2ba854587
jani.maenpaa <jani.maenpaa@aalto.fi>
2024-02-25 20:23:24 +0200

    Refactor nations

--- src/routes/nations.ts
+++ src/routes/nations.ts
@@ -1,11 +1,12 @@
 import { Router, Request, Response } from 'express'
 import { getNations } from '../services/nations'
+import { handleServiceResponse } from '../utils/httpHandlers'
 
 const router = Router()
 
 router.get('/api/nations', async (_req: Request, res: Response) => {
-  const data = await getNations()
-  res.json(data)
+  const serviceResponse = await getNations()
+  handleServiceResponse(serviceResponse, res)
 })
 
 export default router
--- src/services/nations.ts
+++ src/services/nations.ts
@@ -1,6 +1,18 @@
+import { Nation } from '@prisma/client'
 import prisma from '../../prisma/prisma-client'
+import { ResponseStatus, ServiceResponse } from '../../src/models/serviceResponse'
 
 export const getNations = async () => {
-  const nations = await prisma.nation.findMany()
-  return nations
+  try {
+    const nations = await prisma.nation.findMany()
+
+    if (!nations) {
+      return new ServiceResponse(ResponseStatus.Failed, 'No nations found', null, 404)
+    }
+
+    return new ServiceResponse<Nation[]>(ResponseStatus.Success, 'Nations found', nations, 200)
+  } catch (error) {
+    const errorMessage = 'Error getting nations: ' + error
+    return new ServiceResponse(ResponseStatus.Failed, errorMessage, null, 500)
+  }
 }
